18be2bc7b1915aeed7e2c9ecf81c1e267149977a
Stefan van Echtelt <stefan@van-echtelt.nl>
2024-11-30 09:03:20 +0100

    Verplaatsen van alle files naar nivauo hoger

--- spring-demo/Spring demo/src/main/java/com/example/spring_demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_demo.repository;
-
-import com.example.spring_demo.domain.Product;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
