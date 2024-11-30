4690d75ac999c7eac2c42471a2ae52dfdfa89cb6
Stefan van Echtelt <stefan@van-echtelt.nl>
2024-11-30 09:00:15 +0100

    Change startup of testcontainers

--- spring-demo/Spring demo/src/main/java/com/example/spring_demo/domain/listeners/ProductListener.java
+++ spring-demo/Spring demo/src/main/java/com/example/spring_demo/domain/listeners/ProductListener.java
@@ -5,6 +5,7 @@ import com.example.spring_demo.kafka.KafkaProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostRemove;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,6 +20,18 @@ public class ProductListener {
     @PostPersist
     public void afterInsert(Product product) throws JsonProcessingException {
         String json = mapper.writeValueAsString(product);
+        kafkaProducer.send(TOPIC, json);
+    }
+
+    @PostRemove
+    public void afterRemove(Product product) throws JsonProcessingException {
+        Product deletedProduct = new Product();
+        deletedProduct.setId(product.getId());
+
+        String json = mapper.writeValueAsString(deletedProduct);
+
+        // TODO is this empty deadletter
+
         kafkaProducer.send(TOPIC, json);
     }
 }
--- spring-demo/Spring demo/src/test/java/com/example/spring_demo/integratieTests/AbstractIntegrationTest.java
+++ spring-demo/Spring demo/src/test/java/com/example/spring_demo/integratieTests/AbstractIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.example.spring_demo.integratieTests;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.KafkaContainer;
 
@@ -20,16 +18,16 @@ public class AbstractIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
-    @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
+    protected static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine");
 
-    @Container
-    static KafkaContainer kafka = new KafkaContainer("apache/kafka")
+    protected static KafkaContainer kafka = new KafkaContainer("apache/kafka")
             .withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "true");
 
     @BeforeAll
     public static void init() {
+        postgres.start();
+        kafka.start();
         System.setProperty("spring.kafka.bootstrap-servers", kafka.getBootstrapServers());
     }
 }
